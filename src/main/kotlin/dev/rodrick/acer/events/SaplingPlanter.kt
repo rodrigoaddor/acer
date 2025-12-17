daa177b617fe6166dfa0ac5d30b8e10a0c20474c
Rodrigo Addor <me@rodr.dev>
2025-12-17 12:59:56 -0300

    feat: refactor to use fabric entity unload event

--- src/main/kotlin/dev/rodrick/acer/events/SaplingPlanter.kt
+++ src/main/kotlin/dev/rodrick/acer/events/SaplingPlanter.kt
@@ -1,10 +1,9 @@
 package dev.rodrick.acer.events
 
 import dev.rodrick.acer.annotations.Init
-import dev.rodrick.acer.callbacks.EntityDespawnCallback
 import dev.rodrick.acer.config.AcerConfig
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents
 import net.minecraft.core.particles.ParticleTypes
-import net.minecraft.server.level.ServerLevel
 import net.minecraft.tags.ItemTags
 import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.item.BlockItem
@@ -14,21 +13,18 @@ object SaplingPlanter {
     private val SAPLINGS = ItemTags.SAPLINGS
 
     @Init
-    fun init() = EntityDespawnCallback.EVENT.register { entity ->
+    fun init() = ServerEntityEvents.ENTITY_UNLOAD.register { entity, level ->
         val (enabled, chance) = AcerConfig.data.replantSaplings
 
         if (enabled) {
             if (entity is ItemEntity && entity.isSapling && entity.inBlockState.isAir) {
                 val stack = entity.item
                 val block = (stack.item as BlockItem).block
-                val world = entity.level()
 
-                if (world is ServerLevel && block.defaultBlockState()
-                        .canSurvive(entity.level(), entity.blockPosition())
-                ) {
+                if (block.defaultBlockState().canSurvive(entity.level(), entity.blockPosition())) {
                     if (chance >= 1 || Math.random() >= (1 - chance).pow(stack.count)) {
-                        world.setBlockAndUpdate(entity.blockPosition(), block.defaultBlockState())
-                        world.sendParticles(
+                        level.setBlockAndUpdate(entity.blockPosition(), block.defaultBlockState())
+                        level.sendParticles(
                             ParticleTypes.HAPPY_VILLAGER,
                             entity.blockX + .5,
                             entity.blockY + .5,
